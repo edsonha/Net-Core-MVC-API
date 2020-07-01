b925ca837bf3b0c69c98372485f3381747cb0733
edsonha <edsonha@gmail.com>
2020-07-01 18:31:18 +0800

    Setting up DBContext and establish connection strings before migration

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Commander.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Commander
 {
@@ -26,6 +27,7 @@ namespace Commander
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<CommanderContext>(option => option.UseSqlServer(Configuration.GetConnectionString("CommanderConnection")));
       services.AddControllers();
       services.AddScoped<ICommanderRepo, MockCommanderRepo>();
     }
