70241dbc2a0c2cd716ce3035037a14f3c30ba3e5
edsonha <edsonha@gmail.com>
2020-07-02 10:44:48 +0800

    Part 2 of PATCH - update controller

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -4,6 +4,7 @@ using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
 using Commander.Dtos;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace Commander.Controllers
 {
@@ -64,6 +65,33 @@ namespace Commander.Controllers
         return NotFound();
       }
       _mapper.Map(commandUpdateDto, commandModelFromRepo);
+
+      _repository.UpdateCommand(commandModelFromRepo);
+
+      _repository.SaveChanges();
+
+      return NoContent();
+    }
+
+    //PATCH api/commands/{id}
+    [HttpPatch("{id}")]
+    public ActionResult PartialCommandUpdate(int id, JsonPatchDocument<CommandUpdateDto> patchDoc)
+    {
+      var commandModelFromRepo = _repository.GetCommandById(id);
+      if (commandModelFromRepo == null)
+      {
+        return NotFound();
+      }
+
+      var commandToPatch = _mapper.Map<CommandUpdateDto>(commandModelFromRepo);
+      patchDoc.ApplyTo(commandToPatch, ModelState);
+
+      if (!TryValidateModel(commandToPatch))
+      {
+        return ValidationProblem(ModelState);
+      }
+
+      _mapper.Map(commandToPatch, commandModelFromRepo);
 
       _repository.UpdateCommand(commandModelFromRepo);
 
